5690cdc4bf618b25dc6b64f1fd1ef41b0680067b
Tomasz Blok <s22394@pjwstk.edu.pl>
2023-11-21 00:21:25 +0100

    kedro autogluon

---
+++ asi-05/src/asi_05/pipelines/training.py
@@ -0,0 +1,18 @@
+from sklearn.model_selection import train_test_split
+import pickle
+
+from autogluon.tabular import TabularPredictor
+import pandas as pd
+import numpy as np
+import numpy as np
+import pandas as pd
+
+
+def autogluonTraining(data):
+    train_X, test_X = train_test_split(pd.DataFrame(data))
+
+    predictor = TabularPredictor(label='planet_type').fit(pd.DataFrame(train_X))
+    predictions = predictor.predict(pd.DataFrame(test_X))
+
+    pickle.dump(predictions, open('model.sav', 'wb'))
+    print("Model successfully saved")
