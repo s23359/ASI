9424c6ef6b18ab46c86a2d8f6a289f5015109fc7
Tomasz Blok <s22394@pjwstk.edu.pl>
2024-01-16 09:27:18 +0100

    Moved kedro session running to backend

--- asi-05/app.py
+++ asi-05/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException, Request
 from autogluon.tabular import TabularPredictor
+from kedro.framework.startup import bootstrap_project
+from kedro.framework.session import KedroSession
 import pandas as pd
 from datetime import datetime
 from pathlib import Path
@@ -7,30 +9,42 @@ from pathlib import Path
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def on_startup():
-    global predictor
+# @app.on_event("startup")
+# async def on_startup():
 
-    model_directory = Path('AutogluonModels')
-    if not model_directory.exists():
-        raise FileNotFoundError(f"Nie znaleziono katalogu modelu: {model_directory}")
-
-    # Znajdowanie najnowszego modelu
-    latest_model_path = max(model_directory.iterdir(), key=lambda x: x.stat().st_mtime)
-
-    if not latest_model_path.is_dir():
-        raise FileNotFoundError(f"Nie znaleziono modelu: {latest_model_path}")
-    
-    print(latest_model_path)
-
-    predictor = TabularPredictor.load(str(latest_model_path))
 
 @app.post("/predict")
 async def predict(request: Request):
     try:
+        global predictor
+
+        model_directory = Path('AutogluonModels')
+        if not model_directory.exists():
+            raise FileNotFoundError(f"Nie znaleziono katalogu modelu: {model_directory}")
+
+        # Znajdowanie najnowszego modelu
+        latest_model_path = max(model_directory.iterdir(), key=lambda x: x.stat().st_mtime)
+
+        if not latest_model_path.is_dir():
+            raise FileNotFoundError(f"Nie znaleziono modelu: {latest_model_path}")
+    
+        print(latest_model_path)
+
+        predictor = TabularPredictor.load(str(latest_model_path))
         input_data = await request.json()
         data = pd.DataFrame([input_data])
         prediction = predictor.predict(data)
         return {"prediction": prediction.tolist()}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/run")
+async def run(request: Request):
+    try:
+        project_path = Path.cwd()
+        bootstrap_project(project_path)
+        with KedroSession.create(project_path) as session:
+            session.run(pipeline_name="__default__")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- asi-05/streamlit_app.py
+++ asi-05/streamlit_app.py
@@ -1,62 +1,59 @@
 import streamlit as st
-from kedro.framework.startup import bootstrap_project
-from kedro.framework.session import KedroSession
 from pathlib import Path
 import requests
 
+PREDICT = "http://localhost:8000/predict"
+RUN = "http://localhost:8000/run"
 
-# Załadowanie kontekstu Kedro
-project_path = Path.cwd()
-bootstrap_project(project_path)
+st.title('Aplikacja Streamlit do Uruchamiania Potoków Kedro')
 
-FASTAPI_ENDPOINT = "http://localhost:8000/predict"
-
-with KedroSession.create(project_path) as session:
-    st.title('Aplikacja Streamlit do Uruchamiania Potoków Kedro')
-
-    if st.button('Uruchom Potok Kedro'):
-        session.run(pipeline_name="__default__")
+if st.button('Uruchom Potok Kedro'):
+    response = requests.get(RUN)
+    st.success('Potok został uruchomiony!')
+    if response.status_code == 200:
         st.success('Potok został uruchomiony!')
+    else:
+        st.error("Błąd podczas wykonywania uruchamiania potoku")
 
-    with st.form(key='predict_form'):
-        name = st.text_input('name', value="")
-        distance = st.text_input('distance', value="")
-        stellar_magnitude = st.text_input('stellar_magnitude', value="")
-        discovery_year = st.text_input('discovery_year', value="")
-        mass_multiplier = st.text_input('mass_multiplier', value="")
-        mass_wrt = st.text_input('mass_wrt', value="")
-        radius_multiplier = st.text_input('radius_multiplier', value="")
-        radius_wrt = st.text_input('radius_wrt', value="")
-        orbital_radius = st.text_input('orbital_radius', value="")
-        orbital_period = st.text_input('orbital_period', value="")
-        eccentricity = st.text_input('eccentricity', value="")
-        detection_method = st.text_input('detection_method', value="")
-        submit_button = st.form_submit_button('Wykonaj Predykcję')
+with st.form(key='predict_form'):
+    name = st.text_input('name', value="")
+    distance = st.text_input('distance', value="")
+    stellar_magnitude = st.text_input('stellar_magnitude', value="")
+    discovery_year = st.text_input('discovery_year', value="")
+    mass_multiplier = st.text_input('mass_multiplier', value="")
+    mass_wrt = st.text_input('mass_wrt', value="")
+    radius_multiplier = st.text_input('radius_multiplier', value="")
+    radius_wrt = st.text_input('radius_wrt', value="")
+    orbital_radius = st.text_input('orbital_radius', value="")
+    orbital_period = st.text_input('orbital_period', value="")
+    eccentricity = st.text_input('eccentricity', value="")
+    detection_method = st.text_input('detection_method', value="")
+    submit_button = st.form_submit_button('Wykonaj Predykcję')
 
-    if submit_button:
+if submit_button:
 
-        # Przygotowanie danych do predykcji
-        data_to_predict = {
-            'name': name,
-            'distance': distance,
-            'stellar_magnitude': stellar_magnitude,
-            'discovery_year': discovery_year,
-            'mass_multiplier': mass_multiplier,
-            'mass_wrt': mass_wrt,
-            'radius_multiplier': radius_multiplier,
-            'radius_wrt': radius_wrt,
-            'orbital_radius': orbital_radius,
-            'orbital_period': orbital_period,
-            'eccentricity': eccentricity,
-            'detection_method': detection_method
-        }
-        print(data_to_predict)
+    # Przygotowanie danych do predykcji
+    data_to_predict = {
+        'name': name,
+        'distance': distance,
+        'stellar_magnitude': stellar_magnitude,
+        'discovery_year': discovery_year,
+        'mass_multiplier': mass_multiplier,
+        'mass_wrt': mass_wrt,
+        'radius_multiplier': radius_multiplier,
+        'radius_wrt': radius_wrt,
+        'orbital_radius': orbital_radius,
+        'orbital_period': orbital_period,
+        'eccentricity': eccentricity,
+        'detection_method': detection_method
+    }
+    print(data_to_predict)
 
-        # Wysyłanie żądania do FastAPI
-        response = requests.post(FASTAPI_ENDPOINT, json=data_to_predict)
+    # Wysyłanie żądania do FastAPI
+    response = requests.post(PREDICT, json=data_to_predict)
 
-        if response.status_code == 200:
-            prediction = response.json()
-            st.success(f"Wynik Predykcji: {prediction}")
-        else:
-            st.error("Błąd podczas wykonywania predykcji")
+    if response.status_code == 200:
+        prediction = response.json()
+        st.success(f"Wynik Predykcji: {prediction}")
+    else:
+        st.error("Błąd podczas wykonywania predykcji")
